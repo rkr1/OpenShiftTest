7c7467c5e73170b15c110eaec9ede052ad6de656
Renee Rodgers <rkr1@users.noreply.github.com>
2018-02-10 10:18:29 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+//  OpenShift sample Node application
+
+var express = require('express'),
+    app     = express();
+
+var request = require('request'); // "Request" library
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+    
+app.use(express.static(__dirname + '/public'))
+   .use(cookieParser());
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1' || '0.0.0.0';
+
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
